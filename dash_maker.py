e05d8f5754463103d7bddc69c9c2fd3009b31b87
Da-artof <85499672+Da-artof@users.noreply.github.com>
2026-05-18 21:49:10 +0100

    Add radio button to filter by region

--- dash_maker.py
+++ dash_maker.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -11,10 +11,25 @@ fig = px.line(df, x='date', y='sales')
 app.layout = html.Div(children=[
     html.H1(children='Sales visualiser'),
 
+    html.Label('Regions'),
+    dcc.RadioItems(['north', 'east', 'south', 'west', 'all'],
+                   'all', id='region'),
+
     dcc.Graph(
         id='Sales graph',
         figure=fig
     )
-])
+], className='app-header')
+
+
+@callback(
+    Output(component_id='Sales graph', component_property='figure'),
+    Input(component_id='region', component_property='value')
+)
+def update_graph(region):
+    if region == 'all':
+        return px.line(df, x='date', y='sales')
+    return px.line(df[df['region'] == region], x='date', y='sales')
+
 
 app.run()
